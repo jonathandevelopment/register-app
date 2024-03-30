3e3ce76c72ab4854751655aa33d299df985d415f
Jonathancodesurfer <jonathanalpizar43@gmail.com>
2024-03-30 11:49:54 -0600

    added fucnt

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -4,19 +4,19 @@ import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { type User } from '@supabase/supabase-js'
 import { createClient } from '../../../../utils/supabase/client'
+import Avatar from './avatar'
+import Link from 'next/link'
 
 const navigation = [
-  { name: 'Dashboard', href: '/', current: true },
+  { name: 'Reservas', href: '/', current: true },
   { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
+  { name: 'Perfil de Atleta', href: '#', current: false },
+  { name: 'Calendario', href: '#', current: false },
 ]
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
 }
-
-
 
 export default function Navbar({ user }: { user: User | null }) {
 
@@ -55,7 +55,6 @@ export default function Navbar({ user }: { user: User | null }) {
     getProfile()
   }, [user, getProfile])
 
-  console.log(`url: ${avatar_url}`)
 
   return (
     <Disclosure as="nav" className="bg-gray-800">
@@ -110,12 +109,16 @@ export default function Navbar({ user }: { user: User | null }) {
                     <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="absolute -inset-1.5" />
                       <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src={avatar_url || 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'}
-                        alt=""
+                     
+                      <Avatar
+                          uid={user?.id ?? null}
+                          url={avatar_url}
+                          size={50}
+                          onUpload={(url) => {
+                              setAvatarUrl(url)
+                              // updateProfile({ name, lastname, website, avatar_url: url })
+                          }}
                       />
-                      <div className='text-white ml-2'>{name}</div>
                     </Menu.Button>
                   </div>
                   <Transition
@@ -130,22 +133,12 @@ export default function Navbar({ user }: { user: User | null }) {
                     <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item>
                         {({ active }) => (
-                          <a
-                            href="#"
+                          <Link
+                            href="/account"
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
-                            Your Profile
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Settings
-                          </a>
+                            Perfil del Atleta
+                          </Link>
                         )}
                       </Menu.Item>
                       <Menu.Item>
